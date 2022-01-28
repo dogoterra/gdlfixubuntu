0e29b5c367cd056ac6f9357a384cd4716f0041dc
Dogo <47029530+dogoterra@users.noreply.github.com>
2022-01-28 04:31:53 +0100

    Update autofix.sh

--- autofix.sh
+++ autofix.sh
@@ -1,8 +1,7 @@
 sudo apt update && sudo apt upgrade
-sudo apt install wget
 sudo rm /etc/apt/apt.conf.d/01ubuntu
 sudo rm /etc/apt/preferences
-sudo wget https://raw.githubusercontent.com/dogoterra/gldfixubuntu/main/files/01ubuntu -O /etc/apt/apt.conf.d/01ubuntu
-sudo wget https://raw.githubusercontent.com/dogoterra/gldfixubuntu/main/files/preferences -O /etc/apt/preferences
-sudo wget https://raw.githubusercontent.com/dogoterra/gldfixubuntu/main/files/gld.sources.list -O /etc/apt/sources.list.d/gld.sources.list
+sudo wget https://raw.githubusercontent.com/dogoterra/gdlfixubuntu/main/files/01ubuntu -O /etc/apt/apt.conf.d/01ubuntu
+sudo wget https://raw.githubusercontent.com/dogoterra/gdlfixubuntu/main/files/preferences -O /etc/apt/preferences
+sudo wget https://raw.githubusercontent.com/dogoterra/gdlfixubuntu/main/files/gdl.sources.list -O /etc/apt/sources.list.d/gdl.sources.list
 sudo apt update
